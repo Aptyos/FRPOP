a675fddcc53b8e55fc1b65cd815bcb619778c19a
Aptyos <maltese.jean-yves@orange.fr>
2017-02-19 11:26:40 +0100

    frpop2016

--- server.R
+++ server.R
@@ -8,34 +8,40 @@
 #
 
 library(shiny)
+library(leaflet)
+library(dplyr)
+library(RColorBrewer)
 
-# Define server logic required to draw a histogram
+xlib <- c("ggplot2", "ggmap", "rgdal", "rgeos", "maptools", "dplyr", "tidyr", "sp", "tmap")
+lapply(xlib, library, character.only = TRUE)
+
+dv <- read.csv("dep_ville.csv", sep=";")
+
+tmp <- read.csv("depPop.csv")
+deptPop <-
+  tmp %>%
+  group_by( cod_dept) %>%
+  summarise(superficie=sum(area), pop=sum(pop) ) %>%
+  mutate(ds = 100*ceiling(pop/superficie) ) %>%
+  arrange(cod_dept)
+
+rm(tmp)
+
+
 shinyServer(function(input, output) {
   
-  dv <- read.csv("dep_ville.csv", sep=";")
-  tmp <- read.csv("depPop.csv")
-  deptPop <- 
-    tmp %>%
-    group_by( cod_dept) %>%
-    summarise(superficie=sum(area), pop=sum(pop) ) %>% 
-    mutate(ds = 100*ceiling(pop/superficie) ) %>%
-    arrange(cod_dept) 
-  
-  rm(tmp)
-
-  
-  p_rds = readRDS("DEPARTEMENT/jym93.Rds")
+  p_rds = readRDS("jym93.Rds")
   p_rds@data = left_join(p_rds@data, deptPop, by=c("CODE_DEPT"="cod_dept"))
   
-  comm93 <- readRDS("DEPARTEMENT/comm93.Rds")
-  compop <- data.frame (code_ins = comm93$INSEE_COM, 
-                        dsty = ceiling(100*as.numeric(levels(comm93$POPULATION))[comm93$POPULATION] 
-                                       /as.numeric(levels(comm93$SUPERFICIE))[comm93$SUPERFICIE] ) )
+  comm93 <- readRDS("comm93.Rds")
+  compop <- data.frame (code_ins = comm93$INSEE_COM,
+                        dsty = 100*as.numeric(levels(comm93$POPULATION))[comm93$POPULATION]
+                        /as.numeric(levels(comm93$SUPERFICIE))[comm93$SUPERFICIE] )
   comm93@data = left_join(comm93@data, compop, by=c("INSEE_COM"="code_ins"))
   
   binpal <- colorBin("YlOrRd", comm93$dsty, 5, pretty = TRUE,alpha = TRUE)
   
-  colorpal <- reactive({  colorNumeric("YlOrRd", comm93$dsty) })
+  colorpal <- reactive({ colorNumeric("YlOrRd", comm93$dsty) })
   
   output$dep_name <- renderText({
     paste(p_rds[p_rds$CODE_DEPT==input$deptno,]$NOM_DEPT," ")
@@ -57,42 +63,42 @@ shinyServer(function(input, output) {
   
   output$map = renderLeaflet({
     leaflet()     %>%
-      addTiles()    %>% 
-      setView(lng = dv[ dv$CODE_DEPT== input$deptno,]$longitude, 
-              lat = dv[ dv$CODE_DEPT== input$deptno,]$latitude , 
-              zoom= 7) 
+      addTiles()    %>%
+      setView(lng = dv[ dv$CODE_DEPT== input$deptno,]$longitude,
+              lat = dv[ dv$CODE_DEPT== input$deptno,]$latitude ,
+              zoom= 9)
   })
   
   
   observe({
-    leafletProxy("map") %>%
-      clearShapes()  %>%
-      addPolygons(data = p_rds[ p_rds$CODE_DEPT == input$deptno, ] ,
-                  weight = 2,
-                  fillColor = "green", 
-                  popup = paste(p_rds[ p_rds$CODE_DEPT == input$deptno, ]$NOM_CHF, 
-                                as.character(p_rds[ p_rds$CODE_DEPT == input$deptno, ]$Total) )
-      ) 
-  })
-  
-  observeEvent(input$frDensity, {
-    pxy <- leafletProxy("map", data = comm93[ comm93$CODE_DEPT == input$deptno, ])
-    pxy %>%
-      #clearControls() %>%
-      clearShapes() %>%
-      addPolygons(weight = 1,
-                  smoothFactor = 0.2, 
-                  fillOpacity = 0.8, 
-                  color = ~binpal(dsty)
-      ) %>%
-      #pxy %>%
-      clearControls()
-    if (input$legend) {
+    
+    if (input$frDensity == 0 ) {
+      
+      leafletProxy("map") %>%
+        clearShapes()  %>%
+        addCircles(lng=dv[dv$CODE_DEPT== input$deptno,]$longitude, lat=dv[ dv$CODE_DEPT== input$deptno,]$latitude, weight =12, color= "red")  %>%
+        addPolygons(data = p_rds[ p_rds$CODE_DEPT == input$deptno, ] ,
+                    weight = 2, fillColor = "green",
+                    popup = paste(p_rds[ p_rds$CODE_DEPT == input$deptno, ]$NOM_CHF,
+                                  as.character(p_rds[ p_rds$CODE_DEPT == input$deptno, ]$Total) )
+        )
+      
+    } else {
+      pxy <- leafletProxy("map", data = comm93[ comm93$CODE_DEPT == input$deptno, ])
+      pxy %>%
+        clearShapes() %>%
+        addPolygons(weight = 1,
+                    smoothFactor = 0.2,
+                    fillOpacity = 0.8,
+                    color = ~binpal(dsty)
+        ) %>%
+        clearControls()
       pal <- colorpal()
-      pxy %>% 
-        addLegend(position = "bottomright", pal = pal, values = ~ dsty)
+      
+      pxy %>%
+        addLegend(position = "bottomright", pal = pal, values = ~ dsty, title="Area Density", opacity =1.5)
+      
     }
-    #clearControls()
   })
   
-})
+}) # END SERVER
--- ui.R
+++ ui.R
@@ -1,48 +1,48 @@
+#
+# This is the user-interface definition of a Shiny web application. You can
+# run the application by clicking 'Run App' above.
+#
+# Find out more about building applications with Shiny here:
+# 
+#    http://shiny.rstudio.com/
+#
+
 library(shiny)
-library(leaflet)
-library(dplyr)
-library(RColorBrewer)
 
-xlib <- c("ggmap", "rgdal", "rgeos", "maptools", "dplyr", "tidyr", "sp", "tmap")
-lapply(xlib, library, character.only = TRUE)
-
-
-library(shiny)
-
-# Define UI for application that draws a histogram
-# shinyUI(fluidPage(
-shinyUI(bootstrapPage(
-  tags$style(type = "text/css", "html, body {width:100%;height:100%}"),  
+# Define UI
+shinyUI( 
+  fluidPage(
+  
+  # Application title
+  titlePanel("French Population in 2016"),
+  
   sidebarPanel(
-    h2("Liste des Departements"),
-    selectInput(inputId   = "deptno", 
-                label= "Choose a Department Number",
-                choices=c("01","02","2A","2B","03","04","05","06","07","08","09","10","11","12","13","14","15","16","17","18","19",
-                          "2A","2B","21","22","23","24","25","26","27","28","29","30","31","32","33","34","35","36","37",
-                          "38","39","40","41","42","43","44","45","46","47","48","49","50","51","52","53","54","55","56",
-                          "57","58","59","60","61","62","63","64","65","66","67","68","69","70","71","72","73","74","75",
-                          "76","77","78","79","80","81","82","83","84","85","86","87","88","89","90","91","92","93","94",
-                          "95"),        
-                selected  = 'All'),
-    submitButton("Submit"),
-    h4("Nom Departement"),
+    width = 2,
+    h3("Departements"),
+    selectInput(inputId="deptno",
+                label= "Department Number",
+                choices=c("01","02","2A","2B","03","04","05","06","07","08","09",c(10:95)),
+                selected  = "75"),
+    h4("Departement Name"),
     textOutput("dep_name"),
     tags$head(tags$style("#dep_name{color: blue; font-size: 12px; font-style: italic; }" ) ),
-    h4("Ville Principale"),
+    h4("Main City"),
     textOutput("city_name"),
     tags$head(tags$style("#city_name{color: blue; font-size: 12px; font-style: italic; }" ) ),
-    h4("Latitude et Longitude"),
+    h4("Latitude and Longitude"),
     textOutput("LatitudeLongitude"),
     tags$head(tags$style("#LatitudeLongitude{color: blue; font-size: 12px; font-style: italic; }" ) ),
-    h4("Population Density"),
+    h4("Average Dept. Density"),
     textOutput("density"),
     tags$head(tags$style("#density{color: blue; font-size: 12px; font-style: italic; }" ) ),
-    actionButton(inputId = "frDensity","Overall Density"),
-    checkboxInput("legend", "Show legend", TRUE)
+    # checkboxInput(inputId = "legend", "Show legend", TRUE),
+    checkboxInput(inputId = "frDensity","Overall Density",value= FALSE),
+    h4(""),
+    submitButton("Submit")
   ),
   # Show a plot of the generated distribution
   mainPanel(
-    leafletOutput("map") #, width = "100%", height = "100%")
+    leafletOutput("map", width = 700, height = 700) #, width = "100%", height = "100%") 
+    )
   )
-  
-))
+)
